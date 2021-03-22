c8b8bafdefa7d71fef0b5527041dc8e9aaaa9bfa
danielcalderon-ux <danielcal89@hotmail.com>
2021-03-22 18:12:19 +0000

    final

--- src/js/index.js
+++ src/js/index.js
@@ -29,18 +29,34 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  if (variables.name === null) {
+    variables.name = "Daniel";
+  }
+  if (variables.lastname === null) {
+    variables.lastname = "Calderon";
+  }
+  if (variables.role === null) {
+    variables.role = "Developer";
+  }
+  if (variables.city === null) {
+    variables.city = "San Jose";
+  }
+  if (variables.country === null) {
+    variables.country = "Costa Rica";
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -56,12 +72,13 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://thumbs.dreamstime.com/z/personaje-de-dibujos-animados-de-la-rana-91709519.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "/danielcalderon-ux",
     linkedin: null,
     instagram: null,
     name: null,
