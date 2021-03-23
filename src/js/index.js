35924d3246d6bfe7d996b7a763df423eb1cbf344
danielcalderon-ux <danielcal89@hotmail.com>
2021-03-23 03:17:03 +0000

    correcion en la url

--- src/js/index.js
+++ src/js/index.js
@@ -53,10 +53,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com${variables.github}"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
