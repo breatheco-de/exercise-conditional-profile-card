3949e1c49ffe736093d12ca86dbc2a0aedf589ad
xaviune <xaviune@gmail.com>
2022-11-21 20:20:45 +0000

    misspell fixed

--- src/js/app.js
+++ src/js/app.js
@@ -85,7 +85,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new valus
+      render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
 };
