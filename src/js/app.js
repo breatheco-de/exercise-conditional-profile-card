475eac84112ef7d8feebe0768bd618563a2440b2
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-04 09:10:05 +0000

    ejercicio corregido

--- src/js/app.js
+++ src/js/app.js
@@ -41,10 +41,18 @@ function render(variables = {}) {
     variables.country == null ? "Country" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-          <li><a href="https://twitter.com/elonmusk"><i class="fa-brands fa-twitter"></i></a></li>
-            <li><a href="https://github.com/ginajohana"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/in/jesus-famoso"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/dualipa"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="https://twitter.com/${
+            variables.twitter
+          }"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }<i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
