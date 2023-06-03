734682f573cd020231dd84d3d04ef5f38b519fc0
Khrise Franchesca <129009072+Khrisefzm@users.noreply.github.com>
2023-06-03 02:03:53 +0000

    Change variable for ul class

--- src/js/app.js
+++ src/js/app.js
@@ -39,7 +39,7 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
