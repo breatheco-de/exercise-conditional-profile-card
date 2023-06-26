e311ce810e22cf67dc4a3c7e6c6a5e312a6fc2ab
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-06-26 23:37:51 +0000

    updated

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,57 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  let cover = "";
+  if (variables.includeCover) {
+    cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  } else {
+    cover = "<div class='cover'></div>";
+  }
+
+  let socialMediaPosition = "position-left";
+  if (variables.socialMediaPosition === "position-right") {
+    socialMediaPosition = "position-right";
+  }
+
+  const profileHTML = `
+        <div class="widget">
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Lucy"} ${
+    variables.lastname ? variables.lastname : "Boilett"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Web Developer"}</h2>
+          <h3>${variables.city ? variables.city + ", " : ""}${
+    variables.country ? variables.country : "USA"
+  }</h3>
+          <ul class="${socialMediaPosition}">
+            ${
+              variables.twitter
+                ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.github
+                ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.linkedin
+                ? `<li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.instagram
+                ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+                : ""
+            }
           </ul>
         </div>
-    `;
+      `;
+
+  document.querySelector("#widget_content").innerHTML = profileHTML;
 }
 
 /**
