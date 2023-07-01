2b29e5d7834e0ffadfff72b7cd0ab40827d3efe9
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-07-01 00:41:48 +0000

    ProfileCard

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,28 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Eunice" : variables.name} ${
+    variables.lastname == null ? "Hidalgo" : variables.lastname
+  }</h1>
+          <h2>${
+            variables.role == null ? "Health Promoter" : variables.role
+          }</h2>
+          <h3>${
+            variables.country == null ? "Costa Rica" : variables.country
+          }</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href=${
+              variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,16 +68,18 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://w0.peakpx.com/wallpaper/473/145/HD-wallpaper-mang-chimmy-koya-rj-shooky-cooky-in-white-background-bt21.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://i.pinimg.com/originals/b4/27/2b/b4272bb90ce25db80533271eb2233400.gif",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
+    github: "https://github.com/EuniceHG",
+    linkedin: "https://www.linkedin.com/in/eunice-sin-h/",
+    instagram: "https://www.instagram.com/eunice_sin_h/",
     name: null,
     lastname: null,
     role: null,
