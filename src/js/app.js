4d5f32651f34e52d3b719951156bac782954e3a7
Paularicarte <115987177+Paularicarte@users.noreply.github.com>
2023-08-18 19:43:00 +0000

    Cambios en el socialMediaPosition

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name},${variables.lastname}</h1>
+          <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/iniciarsesion?lang=es"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -60,7 +60,7 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
+    twitter: "null",
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
