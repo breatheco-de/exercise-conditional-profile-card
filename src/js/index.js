a4b87c55cb3ab101aab2a236f6c36c73182b1a1e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-20 15:51:35 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -37,18 +37,10 @@ function render(variables = {}) {
               <h2>${variables.role || ""}</h2>
               <h3>${variables.city || ""}, ${variables.country || ""}</h3>
               <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/${
-              variables.twitter ? variables.twitter : "4geeksacademy"
-            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${
-              variables.github ? variables.github : "4geeksacademy"
-            }" target="_blank"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${
-              variables.linkedin ? variables.linkedin : "school/4geeksacademy"
-            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${
-              variables.instagram ? variables.instagram : "4geeksacademy"
-            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter ? variables.twitter : "4geeksacademy"}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ? variables.github : "4geeksacademy"}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin ? variables.linkedin : "school/4geeksacademy"}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : "4geeksacademy"}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
             </div>
         `;
