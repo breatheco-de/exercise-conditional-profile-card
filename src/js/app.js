b876a491910b8d34913e2eba582be68fbbaf9644
Adrian <142854436+withyso@users.noreply.github.com>
2023-10-16 23:38:43 +0000

    Cambio a la variable de country, ahora si es visible

--- src/js/app.js
+++ src/js/app.js
@@ -35,7 +35,7 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
-          <h3>${variables.city}</h3>
+          <h3>${variables.city} ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}"> 
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
