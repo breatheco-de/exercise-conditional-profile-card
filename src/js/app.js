049abd08379a9f578833891653e467e647ab803b
Juan Cruz Pandolfo Martinez <juancruzpandolfo@gmail.com>
2024-10-02 19:29:00 +0000

    mfalta el cambio de background

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,29 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${variables.name == null ? "Lucy" : variables.name}
+              ${variables.lastName == null ? "Boilet" : variables.lastName}</h1>
+              
+              <h2>${variables.role}</h2>
+              <h3>${variables.city} ${variables.country}</h3>
+              <ul class="${variables.socialMediaPosition}">
+                <li><a href="${
+                  variables.twitter
+                }"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="${
+                  variables.github
+                }"><i class="fab fa-github"></i></a></li>
+                <li><a href="${
+                  variables.linkedin
+                }"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="${
+                  variables.instagram
+                }"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+        `;
 }
 
 /**
@@ -68,7 +78,8 @@ window.onload = function() {
     lastName: null,
     role: null,
     country: null,
-    city: null
+    city: null,
+    color: null
   };
   render(window.variables); // render the card for the first time
 
