383b27826d18d9e1ca50c929f8d0d0be18243b31
RdeRausseo <amrausseo20@gmail.com>
2024-11-04 16:08:44 +0000

    RdeRausseo Renderin

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,27 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  variables.name = variables.name !== null || variables.name.length > 0 ? variables.name : "Lucy ";
+  variables.lastName = variables.lastName !== null || variables.lastName.length > 0 ? variables.lastName : "Bettle";
+  variables.role = variables.role !== null || variables.role.length > 0 ? variables.role : "Web Developer"; 
+  variables.city = variables.city !== null || variables.city.length > 0 ? variables.city : "Miami, USA"; 
+  variables.twitter = variables.twitter !== null || variables.twitter.length > 0 ? variables.twitter: "4geeksAcademy"; 
+  variables.github = variables.github !== null || variables.github.length > 0 ? variables.github: "4geeksAcademy";
+  variables.linkedin = variables.twitter !== null || variables.linkedin.length > 0 ? variables.linkedin: "4geeksAcademy";
+  variables.instagram = variables.twitter !== null || variables.instagram.length > 0 ? variables.instagram: "4geeksAcademy";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -49,7 +57,7 @@ function render(variables = {}) {
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
-window.onload = function() {
+window.onload = function () {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
@@ -72,8 +80,8 @@ window.onload = function() {
   };
   render(window.variables); // render the card for the first time
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
+  document.querySelectorAll(".picker").forEach(function (elm) {
+    elm.addEventListener("change", function (e) {
       // <- add a listener to every input
       const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
       let values = {};
@@ -81,10 +89,10 @@ window.onload = function() {
         this.value == "" || this.value == "null"
           ? null
           : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
+            ? true
+            : this.value == "false"
+              ? false
+              : this.value;
       render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
