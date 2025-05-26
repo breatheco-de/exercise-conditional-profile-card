53ec194791fe68a9d07a5e6d2efdd6acc4f5ffda
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-05-26 18:26:53 +0000

    alex

--- src/js/app.js
+++ src/js/app.js
@@ -29,13 +29,14 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  }
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1> ${variables.name} ${variables.lastName} </h1>
+          <h2> ${variables.role} </h2>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
