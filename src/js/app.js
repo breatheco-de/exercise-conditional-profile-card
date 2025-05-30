edea3376edd57165723c4ba78ecf972b7a854d52
Miguel-Olivo-Mercedes <miguelolivomercedes@gmail.com>
2025-05-30 09:21:46 +0000

    perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,11 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name || "Name"} ${variables.lastName ||
+    "Last Name"} </h1>
+          <h2>${variables.role || "Role"} </h2>
+          <h3>${variables.city || "City"}</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -56,7 +57,8 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://i.pinimg.com/736x/0b/f0/28/0bf02848dac44669593426878e5b9c34.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
