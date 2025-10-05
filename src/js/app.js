e59c97c51ee862b9358f4d7d9d44668d10618a3b
jenifferalejandracollazos-droid <jenifferalejandracollazos@gmail.com>
2025-10-05 04:59:05 +0000

    terminadoo

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,36 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  // ubicación
+  let location = "";
+  if (variables.city || variables.country) {
+    location = `${variables.city ? variables.city : ""}${
+      variables.city && variables.country ? ", " : ""
+    }${variables.country ? variables.country : ""}`;
+  }
+
+  // enlaces
+  let socialMediaLinks = "";
+  if (variables.twitter)
+    socialMediaLinks += `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+  if (variables.github)
+    socialMediaLinks += `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`;
+  if (variables.linkedin)
+    socialMediaLinks += `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`;
+  if (variables.instagram)
+    socialMediaLinks += `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name === null ? "" : variables.name} ${
+    variables.lastName === null ? "" : variables.lastName
+  }</h1>
+          <h2>${variables.role === null ? "" : variables.role}</h2>
+          <h3>${location}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            ${socialMediaLinks}
           </ul>
         </div>
     `;
