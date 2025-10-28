927602833d5d826a3bcfa60cf1cf880e1d4594e1
AndreiHanganu <ganganuandrei462@gmail.com>
2025-10-28 13:16:07 +0000

    cambio de nombre

--- src/js/app.js
+++ src/js/app.js
@@ -40,12 +40,8 @@ function render(variables = {}) {
           <h2></h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/${
-              variables.twitter
-            }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${
-              variables.github
-            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
