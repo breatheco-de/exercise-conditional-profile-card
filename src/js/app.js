2c03b0de488c672c02c1debdfa83bd38439b5824
Yovanna Cristina Alvarez <yovanna.alvarez@gmail.com>
2025-10-28 18:13:42 +0000

    posision, twitter, rol

--- src/js/app.js
+++ src/js/app.js
@@ -34,10 +34,10 @@ function render(variables = {}) {
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <h2>${variables.role}</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+          <ul class="${variables.socialMediaPosition}">
+            // <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
